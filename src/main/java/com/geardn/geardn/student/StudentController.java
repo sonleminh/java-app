160f3417423dd2a747d19190d47d8a5975efee2d
Son Le <sonlele2000@gmail.com>
2025-11-16 22:53:23 +0700

    post successfully

--- src/main/java/com/geardn/geardn/student/StudentController.java
+++ src/main/java/com/geardn/geardn/student/StudentController.java
@@ -2,9 +2,7 @@ package com.geardn.geardn.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -23,5 +21,10 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudents() {
         return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
     }
 }
